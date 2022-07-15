7ddd6178306d6ae195b6afa8fd0d7283218365a1
vinnieoh <viniciusoliveira926@gmail.com>
2022-07-15 19:37:44 -0300

    frontend-feito

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
-  return(
-    <h1>Ola mundo</h1>
+  return (
+    <>
+      <ToastContainer />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
-export default App
+export default App;
