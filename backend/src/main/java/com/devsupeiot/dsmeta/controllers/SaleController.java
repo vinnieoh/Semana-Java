61139fe5006d6c250b16762d838e59b25abbdd1b
vinnieoh <viniciusoliveira926@gmail.com>
2022-07-15 23:37:33 -0300

    Twilio sms

--- backend/src/main/java/com/devsupeiot/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsupeiot/dsmeta/controllers/SaleController.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmeta.controllers;
+package com.devsupeiot.dsmeta.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
